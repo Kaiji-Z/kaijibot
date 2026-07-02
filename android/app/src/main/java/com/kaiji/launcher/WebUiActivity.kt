f4218945bf371d8dd58ce143d62d61b73888a8d8
Kaiji-Z <Kaiji-Z@users.noreply.github.com>
2026-07-03 00:56:51 +0800

    android: add missing Log import

--- android/app/src/main/java/com/kaiji/launcher/WebUiActivity.kt
+++ android/app/src/main/java/com/kaiji/launcher/WebUiActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.graphics.Color
 import android.net.http.SslError
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.webkit.SslErrorHandler
